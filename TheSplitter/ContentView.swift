7a56296bd72482c4eb48ac4d2a37a508d3cde925
AdityaK2128 <adityakumar2128@gmail.com>
2023-09-17 14:56:47 +0530

    Initial Commit

---
+++ TheSplitter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheSplitter
+//
+//  Created by Aditya Kumar on 9/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheSplitter/TheSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSplitterApp.swift
+//  TheSplitter
+//
+//  Created by Aditya Kumar on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TheSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
